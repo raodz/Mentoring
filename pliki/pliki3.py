cad9e1fb0c75e254481a8da139753e05d5d2715c
Rafal Odziemski <raodziem@gmail.com>
2023-06-10 19:43:01 +0200

    deleted:    dziedziczenie/dziedziczenie1.py deleted:    dziedziczenie/dziedziczenie2.py deleted:    klasy/klasy1.py deleted:    klasy/klasy2.py deleted:    klasy/klasy3.py deleted:    klasy/klasy4.py deleted:    klasy/klasy5.py deleted:    klasy/klasy6.py deleted:    klasy/klasy7.py deleted:    klasy/klasy8.py deleted:    pliki/dane.txt deleted:    pliki/data.json deleted:    pliki/pliki10.py deleted:    pliki/pliki3.py deleted:    pliki/pliki6.py deleted:    pliki/pliki7.py deleted:    pliki/pliki8.py deleted:    pliki/pliki9.py deleted:    pliki/przyklad.txt deleted:    pliki/przyklad2.txt deleted:    pliki/test.txt deleted:    podstawy/krotki_zbiory.py deleted:    podstawy/listy.py deleted:    podstawy/petle.py deleted:    podstawy/slowniki.py deleted:    struktury_danych/struktury_danych1.py deleted:    struktury_danych/struktury_danych2.py deleted:    struktury_danych/struktury_danych3.py deleted:    struktury_danych/struktury_danych4.py deleted:    struktury_danych/struktury_danych5.py deleted:    struktury_danych/struktury_danych6.py

--- pliki/pliki3.py
+++
@@ -1,6 +0,0 @@
-with open('przyklad.txt', 'r', encoding='utf-8') as file:
-    line0 = file.readline()
-    line1 = file.readline()
-    line2 = file.readline()
-print(line0)
-print(line2)
--- struktury_danych/struktury_danych1.py
+++
@@ -1,9 +0,0 @@
-a = [1, 5, 7]
-b = [2, 3, 2]
-if not set(a) & set(b):
-    print('There is no common element of lists a and b')
-else:
-    print('Lists a and b have a common element')
-
-#  {} -> False
-#  [1] -> True
