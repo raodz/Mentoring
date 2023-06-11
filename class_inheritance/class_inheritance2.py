bc4f7e3e5136f8a16809878aae4521d3f7f7bb50
Rafal Odziemski <raodziem@gmail.com>
2023-06-11 12:54:08 +0200

    feat: add tasks class_inheritance 3-4

--- class_inheritance/class_inheritance2.py
+++ class_inheritance/class_inheritance2.py
@@ -9,20 +9,20 @@ class Depot:
                f' vehicles: {self.vehicles}'
 
 class Vehicle:
-    def __init__(self, number: int, kind: str, depot: Depot, max_velocity: float):
+    def __init__(self, number: int, depot: Depot, max_velocity: float):
         self.number = number
         self.depot = depot
         self.max_velocity = max_velocity
-        self.kind = kind
 
     def __str__(self):
-        return f'This is the {self.kind} nr. {self.number} of the {self.depot.name} ' \
+        return f'This is the {self.__class__.__name__} nr. {self.number} of the' \
+               f' {self.depot.name} ' \
                f'depot. ' \
                f'Its maximal velocity is {self.max_velocity} km/h.'
 
 class Tram(Vehicle):
     def __init__(self, number, depot, max_velocity, cars_number: int):
-        super().__init__(number, 'tram', depot, max_velocity)
+        super().__init__(number, depot, max_velocity)
         self.cars_number = cars_number
 
     def __str__(self):
@@ -30,7 +30,7 @@ class Tram(Vehicle):
 
 class Bus(Vehicle):
     def __init__(self, number, depot, max_velocity, used_fuel: float):
-        super().__init__(number, 'bus', depot, max_velocity)
+        super().__init__(number, depot, max_velocity)
         self.used_fuel = used_fuel
 
     def __str__(self):
@@ -38,7 +38,7 @@ class Bus(Vehicle):
                                    f'used {self.used_fuel} liters of fuel.'
 
 def main():
-    some_vehicle = Vehicle(4, 'tram', Depot('WOLA', 'tram', []), 80)
+    some_vehicle = Vehicle(4, Depot('WOLA', 'tram', []), 80)
     print(some_vehicle)
     some_tram = Tram(24, Depot('WOLA', 'tram', []), 90, 3)
     print(some_tram)
---
+++ class_inheritance/class_inhertance3.py
@@ -0,0 +1,66 @@
+class Member:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return f'Hi, my name is {self.name}'
+
+class Student(Member):
+    def __init__(self, name: str, reason: str):
+        super().__init__(name)
+        self.reason = reason
+
+class Instructor(Member):
+    def __init__(self, name: str, bio: str, skills: list = []):
+        super().__init__(name)
+        self.bio = bio
+        self.skills = skills
+
+    def add_skill(self, skill: str):
+        self.skills.append(skill)
+
+class Workshop:
+    def __init__(self, date: str, subject: str, instructors: list = [],
+                 students: list = []):
+        self.date = date
+        self.subject = subject
+        self.instructors = instructors
+        self.students = students
+
+    def add_participant(self, participant: Member):
+        if participant.__class__.__name__ == 'Instructor':
+            self.instructors.append(participant)
+        else:
+            self.students.append(participant)
+
+    def __str__(self):
+        text = f'Workshop - {self.date} - {self.subject} \n\n' \
+               f'Students \n'
+        students = ''
+        for i, student in enumerate(self.students):
+            students += f'{i+1}. {student.name} - {student.reason} \n'
+        text += students + '\n' + 'Instructors' + '\n'
+        instructors = ''
+        for i, instructor in enumerate(self.instructors):
+            instructors += f'{i+1}. {instructor.name} - ' +\
+                           ', '.join(instructor.skills) + '\n' +\
+                            f'\t {instructor.bio} \n'
+        text += instructors
+        return text
+
+workshop = Workshop("12/03/2014", "Shutl")
+
+jane = Student("Jane Doe", "I am trying to learn programming and need some help")
+lena = Student("Lena Smith", "I am really excited about learning to program!")
+vicky = Instructor("Vicky Python", "I want to help people learn coding.")
+vicky.add_skill("HTML")
+vicky.add_skill("JavaScript")
+nicole = Instructor("Nicole McMillan", "I have been programming for 5 years in Python"
+                                       " and want to spread the love")
+nicole.add_skill("Python")
+
+workshop.add_participant(jane)
+workshop.add_participant(lena)
+workshop.add_participant(vicky)
+workshop.add_participant(nicole)
+print(workshop)
