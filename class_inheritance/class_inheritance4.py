41f2f6842863ae99dc14ad919d91387b474b4b19
Rafal Odziemski <raodziem@gmail.com>
2023-06-12 17:06:46 +0200

    feat: task class_inheritance4 finished

--- class_inheritance/class_inheritance4.py
+++ class_inheritance/class_inheritance4.py
@@ -10,17 +10,19 @@ class Menu:
                '5. Exit'
 
     def get_choice(self):
+        print(self)
         return input('Select an option')
 
 class Submanager:
     def __init__(self, data = []):
         self.data = data
 
-    def add(self, file):
+    def add(self):
+        file = input('Type the file to add')
         self.data.append(file)
 
-    def __str__(self):
-        return self.data
+    def show(self):
+        print(self.data)
 
 class Manager:
     def __init__(self, menu: Menu, notes_sm: Submanager, cards_sm: Submanager):
@@ -32,7 +34,30 @@ class Manager:
         pass  # jak ma działać start?
 
     def show_menu(self):
-        return self.menu
+        return self.menu.__str__()
 
     def execute(self):
-        choice = self.menu.get_choice()
+        options = {'1': self.notes_sm.add, '2': self.cards_sm.add,
+                   '3': self.notes_sm.show, '4': self.cards_sm.show}
+        choice = '1'  # to tylko wartość początkowa
+        while choice != '5':
+            choice = self.menu.get_choice()
+            if choice in ['1', '2', '3', '4']:
+                options[choice]()
+            elif choice == '5':
+                print('Thank you for using the program')
+            else:
+                print('Please provide correct option: 1, 2, 3, 4 or 5')
+
+def main():
+    menu = Menu()
+    notes_sm = Submanager()
+    cards_sm = Submanager()
+    manager = Manager(menu, notes_sm, cards_sm)
+    manager.start()  # co to ma robić?
+    manager.show_menu()
+    manager.execute()
+# problem: 1 i 2 dodają elementy do listy tego samego Submanagera
+
+if __name__ == '__main__':
+    main()
