fe5ed1695b8d21944abda573cac3aa0965cf07ad
Rafal Odziemski <raodziem@gmail.com>
2023-06-12 12:07:26 +0200

    fix: correcting class8

--- class/class8.py
+++ class/class8.py
@@ -1,37 +1,36 @@
 from datetime import datetime
 
 class Case:
+    def __init__(self, name, created_task, end_task):
+        self.name = name
+        self.created_task = created_task
+        self.end_task = end_task
+
+    def retrieve_seconds(self):
+        start = datetime.strptime(self.created_task, '%Y-%m-%dT%H:%M:%S')
+        # kwestia +00:00
+        if self.end_task == None:
+            end = datetime.now()
+        else:
+            end = datetime.strptime(self.end_task, '%Y-%m-%dT%H:%M:%S')
+        return end - start
+
+def main():
     first_case = {
         'name': 'first_case',
-        'created_task': '2021-10-26T19:48:12+00:00',
+        'created_task': '2021-10-26T19:48:12',
         'end_task': None
     }
 
     second_case = {
         'name': 'second_case',
-        'created_task': '2021-09-26 T19:48:12+00:00',
-        'end_task': '2021-10-26T19:48:12+00:00'
+        'created_task': '2021-09-26T19:48:12',
+        'end_task': '2021-10-26T19:48:12'
     }
 
-    def __init__(self):
-        pass
-        # self.name = name
-        # self.created_task = created_task
-        # self.end_task = end_task
-
-    def retrieve_seconds(self, case):
-        start = datetime.strptime(case['created_task'], '%H:%M:%S')
-        if case['end_task'] == None:
-            end = datetime.now()
-        else:
-            end = datetime.strptime(case['end_task'], '%H:%M:%S')
-        return start - end
-
-def main():
-    some_case = Case()
-    print(some_case)
+    for data in [first_case, second_case]:
+        case = Case(data['name'], data['created_task'], data['end_task'])
+        print(case.retrieve_seconds())
 
 if __name__ == '__main__':
     main()
-
-# Do ogarnięcia
