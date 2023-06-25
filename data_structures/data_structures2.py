4355604278e065e2186437e06baa2893aa008f53
Rafal Odziemski <raodziem@gmail.com>
2023-06-25 14:03:34 +0200

    fix: filtering python advanced files only

--- data_structures/data_structures2.py
+++
@@ -1,9 +0,0 @@
-import random
-
-random_numbers = set(random.sample(range(5, 120), 15))
-even_numbers = set()
-for number in random_numbers:
-    if number % 2 == 0:
-        even_numbers.add(number)
-print(random_numbers)
-print(even_numbers)
--- files/files7.py
+++
@@ -1,9 +0,0 @@
-with open('przyklad2.txt', 'r', encoding='utf-8') as file:
-    result = ''
-    for line in file.readlines():
-        splitted_line = line.split(' ')
-        result += splitted_line[0]
-        for i in range(1, len(splitted_line)-1):
-            if splitted_line[i] != splitted_line[i-1] != splitted_line[i-1]:  # Problem z backslashem
-                result += ' ' + splitted_line[i]
-    print(result)
